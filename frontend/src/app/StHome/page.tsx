b80cd64ab123b5ce34b788ece21cd0387d49b1d9
Vaishnavi Rajput <141741075+seduloushaze@users.noreply.github.com>
2024-10-19 16:30:31 +0530

    first commit

---
+++ frontend/src/app/StHome/page.tsx
@@ -0,0 +1,7 @@
+export default function StHome() {
+    return(
+        <div className="flex h-screen w-screen text-9xl">
+            <h1 className="text-center">Home page</h1>
+        </div>
+    )
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import StHome from "./StHome/page";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen text-9xl">
+      <StHome />
+    </div>
+  );
+}
