55873af2839a9f13f4d997137c6931316ccda103
Vaishnavi Rajput <141741075+seduloushaze@users.noreply.github.com>
2024-10-24 18:06:55 +0530

    4th4  commit

--- frontend/src/app/StHome/page.tsx
+++ frontend/src/app/StHome/page.tsx
@@ -1,7 +1,11 @@
+//server side component to create home page for student
+
+import React from "react";
+
 export default function StHome() {
-    return(
-        <div className="flex h-screen w-screen text-9xl">
-            <h1 className="text-center">Home page</h1>
-        </div>
-    )
+  return (
+    <div className="flex h-screen w-screen text-9xl">
+      <h1>Student Home Page</h1>
+    </div>
+  );
 }
